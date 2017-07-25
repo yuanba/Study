7f287028383a51660110de489a20f69f3e70a0e9
GMFTBY <1753485499@qq.com>
2017-07-25 11:28:35 +0800

    Tue Jul 25 11:28:35 CST 2017

--- git-push.sh
+++ git-push.sh
@@ -5,4 +5,5 @@ dating=`date`
 git st    # Git show the status
 git add .   # Git add the changes into the workspace
 git ci -m "$dating"   # commit the changes
-git push origin master > /dev/null &   # push commit into the Github in the back station and kill the output.
+git push origin master &   # push commit into the Github in the back station and kill the output.
+git lg    # show the log
