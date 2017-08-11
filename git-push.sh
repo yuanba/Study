8b1ced35b39a22e258c366177da5c87c6b8e7997
GMFTBY <1753485499@qq.com>
2017-08-11 18:25:57 +0800

    Fri Aug 11 18:25:56 CST 2017 something!

--- git-push.sh
+++ git-push.sh
@@ -4,6 +4,6 @@ dating=`date`
 
 git st    # Git show the status
 git add .   # Git add the changes into the workspace
-git ci -m "$dating"   # commit the changes
+git ci -m "$dating "$1   # commit the changes
 git push origin master &   # push commit into the Github in the back station and kill the output.
 git lg    # show the log
