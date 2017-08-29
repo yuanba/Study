fcb5c980dcfab252a5347c2653e9c25ba04ee931
GMFTBY <18811371908@163.com>
2017-08-29 09:50:27 +0800

    Tue Aug 29 09:50:27 CST 2017 Just Pause for my little courses in the school

--- git-push.sh
+++ git-push.sh
@@ -8,4 +8,3 @@ git commit -m "$dating $1"   # commit the changes
 git push origin master   # push commit into the Github in the back station and kill the output.
 git log    # show the log
 # taqini say 0x29a , No 0x29a , I am so weak :)
-
